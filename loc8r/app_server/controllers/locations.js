ecd0ba43dbf07c60522ab28f5bc7b76a9b0d5eb8
ShinChanU <tlscksdn963@naver.com>
2021-09-24 20:41:26 +0900

    #5 done

--- loc8r/app_server/controllers/locations.js
+++ loc8r/app_server/controllers/locations.js
@@ -1,20 +1,20 @@
 // GET 'home' page
 const homeList = (req, res) => {
-  res.render('index', {
+  res.render('location-list', {
     title: 'Home (ChanWoo Shin_2016410026)'
   });
 };
 
 // GET 'Location info' page
 const locationInfo = (req, res) => {
-  res.render('index', {
+  res.render('location-info', {
     title: 'Location info (ChanWoo Shin_2016410026)'
   });
 };
 
 // GET 'Add review' page
 const addReview = (req, res) => {
-  res.render('index', {
+  res.render('location-review-form', {
     title: 'Add review (ChanWoo Shin_2016410026)'
   });
 };
--- loc8r/app_server/controllers/others.js
+++ loc8r/app_server/controllers/others.js
@@ -1,6 +1,6 @@
 // GET 'about' page
 const about = (req, res) => {
-  res.render('index', {
+  res.render('generic-text', {
     title: 'About (ChanWoo Shin_2016410026)',
   });
 };
