bd473bf3a5c6fd7c51d344569bce0fe3231a5dc4
ShinChanU <tlscksdn963@naver.com>
2021-10-25 19:19:01 +0900

    Rest api using #12

--- loc8r/app_server/controllers/locations.js
+++ loc8r/app_server/controllers/locations.js
@@ -1,31 +1,74 @@
-// GET 'home' page
+const request = require('request');
+
+const apiOptions = {
+  server: 'http://localhost:3000'
+};
+
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = 'http://loc8r1004.herokuapp.com';
+}
+
 const homeList = (req, res) => {
+  const path = '/api/locations';
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'GET',
+    json: {},
+    qs: {
+      lng: 1,
+      lat: 1,
+      maxDistance: 0.002
+      // lng: 127.26411,
+      // lat: 37.00670,
+      // maxDistance: 200000
+    }
+  };
+
+  request(
+    requestOptions,
+    (err, { statusCode }, body) => {
+      let data = [];
+      if (statusCode === 200 && body.length) {
+        data = body.map((item) => {
+          item.distance = formatDistance(item.distance);
+          return item;
+        });
+      }
+      renderHomepage(req, res, data);
+    });
+};
+
+const formatDistance = (distance) => {
+  let thisDistance = 0;
+  let unit = 'm';
+  if (distance > 1000) {
+    thisDistance = parseFloat(distance / 1000).toFixed(1);
+    unit = 'km';
+  } else {
+    thisDistance = Math.floor(distance);
+  }
+  return thisDistance + unit;
+};
+
+const renderHomepage = (req, res, responseBody) => {
+  let message = null;
+  if (!(responseBody instanceof Array)) {
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = "No places found nearby";
+    }
+  }
   res.render('location-list', {
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
       title: 'Loc8r',
-      strapLine: 'Find places to work with wifi near you!'
+      strapLine: 'Find places to work with wifi near you! 2016410026/신찬우'
     },
     sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
-    locations: [{
-      name: 'Starcups',
-      address: '경기도 안성시 중앙로 308',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '3.1km'
-    }, {
-      name: 'Cafe Hero',
-      address: '경기도 안성시 중앙로 274',
-      rating: 4,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '3.2km'
-    }, {
-      name: 'Burger Queen',
-      address: '경기도 안성시 중앙로 389번길 6',
-      rating: 2,
-      facilities: ['Food', 'Premium wifi'],
-      distance: '9.8km'
-    }]
+    locations: responseBody,
+    message
   });
 };
 
